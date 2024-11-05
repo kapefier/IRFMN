f1396ad208039e4a013af6ddce8a6e373a56dc04
Orlando Sagliocco <133375132+kapefier@users.noreply.github.com>
2024-11-05 12:49:19 +0100

    Update t3824h.py

--- t3824h.py
+++ t3824h.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("01_Temperatura.csv")
 df.head()
  
-# filter early hypertemic patients
+# filter Post Cardiac Arrest Pyrexia patients
 uids=pd.read_csv('../UID.csv', usecols=[1])
 df = pd.read_csv("01_Temperatura.csv")
 t38in24_uids = df[(df['VALORE'] >= 38) & (df['DATASTART'] < 1440)]['UID'].tolist()
@@ -15,12 +15,12 @@ uids_sorted=uids.sort_values(by='UID')
 
 # Print the results
 print(f"N of pts with temp recorded: {uids.shape[0]}")
-print(f"Early hyperthermia (temp >= 38 °C within the first 24 hours)")
+print(f"Post Cardiac Arrest Pyrexia (temp >= 38 °C within the first 24 hours)")
 print(f"N of pts: {uids['t38in24h'].sum()} ")
 print(f"Percentage of pts: {((uids['t38in24h'].sum() / uids.shape[0]) * 100):.2f}%")
 
 # save to csv
-uids_sorted.to_csv("filteredtemp.csv", index=False)
+uids_sorted.to_csv("post_cardiacarrest_pyrexia.csv", index=False)
 
 # Set Plot
 labels = ['True', 'False']
@@ -31,5 +31,5 @@ explode = (0.1, 0)
 # Plot
 plt.figure(figsize=(8, 8))
 plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
-plt.title('Early Hypertermia')
+plt.title('Post Cardiac Arrest Pyrexia')
 plt.show()
