1ad68ffafcfb8c50f63931c328cc091faa63c78f
Orlando Sagliocco <133375132+kapefier@users.noreply.github.com>
2024-11-06 17:30:39 +0100

    Update givithon_scripts.py

--- givithon_scripts.py
+++ givithon_scripts.py
@@ -78,3 +78,57 @@ def extract(csv_path, uid_path, output_csv, filter_variable, filter_operator, fi
         plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
         plt.title(filter_value+" "+data_operator+" "+str(data_value)+" minutes")
         plt.show()
+
+def extract2(csv_path, uid_path, output_csv, filter_variable, filter_operator, filter_value, data_variable, data_operator, data_value, show=True):
+    # Load data
+    df = pd.read_csv(csv_path)
+    uids = pd.read_csv(uid_path, usecols=[1])
+    
+    # Dictionary to match operators to functions
+    operators = {
+        '=': lambda x, y: x == y,
+        '>': lambda x, y: x > y,
+        '<': lambda x, y: x < y,
+        '>=': lambda x, y: x >= y,
+        '<=': lambda x, y: x <= y
+    }
+    
+    if filter_operator not in operators or data_operator not in operators:
+        raise ValueError("Invalid operator. Use one of '=', '>', '<', '>=', '<='.")
+    
+    # Apply filter and data conditions
+    filter_condition = operators[filter_operator](df[filter_variable], filter_value)
+    data_condition = operators[data_operator](df[data_variable], data_value)
+    
+    # Apply combined condition
+    filtered_uids = df[filter_condition & data_condition]['UID'].tolist()
+    uids[filter_value] = uids.UID.map(set(filtered_uids).__contains__)
+    
+    # Sort uids by UID
+    uids_sorted = uids.sort_values(by='UID')
+    
+    # Save the sorted DataFrame to a CSV file
+    uids_sorted.to_csv(output_csv, index=False)
+
+    if show:
+        # Print the results
+        total_uids = len(uids)
+        total_true = uids[filter_value].sum()
+        percentage_true = (total_true / total_uids) * 100
+    
+        print(f"N of pts with {filter_variable} recorded: {total_uids}")
+        print(f"Filter applied: {filter_variable} {filter_operator} {filter_value} AND {data_variable} {data_operator} {data_value} minutes")
+        print(f"N of pts: {total_true} ")
+        print(f"Percentage of pts: {percentage_true:.2f}%")
+
+        # Set Plot
+        labels = ['True', 'False']
+        sizes = [total_true, total_uids - total_true]
+        colors = ['red', 'lightgreen']
+        explode = (0.1, 0)
+    
+        # Plot
+        plt.figure(figsize=(4, 4))
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+        plt.title(f"{filter_value} {data_operator} {data_value} minutes")
+        plt.show()
