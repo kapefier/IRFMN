de6d393b25302aa5898f6c034f078e8c31944a09
Orlando Sagliocco <133375132+kapefier@users.noreply.github.com>
2024-11-07 19:55:51 +0100

    __plot as private call operators are self controllo su dtype per evitare errori

--- givithon_scripts_new.py
+++ givithon_scripts_new.py
@@ -1,15 +1,11 @@
-# givithon_scripts.py
+# givithon_scripts_new.py
 
 # Usage example:
-#   extract("../Devices.csv",  "../UID.csv",
-#   "ECMO-a-24h.csv",
+#   extractor = Extractor("../Devices.csv",  "../UID.csv",
 #   "NOMEPRESIDIO", "=", "ECMO",
-#   "DATAINIZIO", "<=", 1440,
-#    show=True)
-
-# Usage example:
-#  csvmerge(['file1', 'file2', 'file3'], 'merged_output')
-#  filenames with no suffix... appending .csv
+#   "DATAINIZIO", "<=", 1440)
+#   extractor.save("output.csv", show=True)
+#
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -35,7 +31,7 @@ class Extractor:
         self.data_operator = data_operator
         self.data_value = data_value
 
-    def extract_and_sync_fs(self, output_csv, filter_value, show=True):
+    def save(self, output_csv, show=True):
         if (self.filter_operator not in self.operators or self.data_operator not in self.operators):
             raise ValueError(
                 "Invalid operator. Use one of '=', '>', '<', '>=', '<='.")
@@ -51,25 +47,34 @@ class Extractor:
         uids_sorted.to_csv(output_csv, index=False)
 
         # side effect : mostrare il plot
-        if (show):
+        if show:
             self.__plot(uids_sorted)
 
-    def extract(self, df, uids, filter_value) -> list[int]:
+    def extract(self, df, uids):
+        # Ensure the filter value and column values are of the same type
+        filter_value = self.filter_value
+        data_value = self.data_value
 
-        filter_condition = self.operators[self.filter_operator](
-            df[self.filter_variable], filter_value)
-        data_condition = self.operators[self.data_operator](
-            df[self.data_variable], self.data_operator)
+        # Convert to appropriate types if necessary
+        if df[self.filter_variable].dtype != type(filter_value):
+            filter_value = df[self.filter_variable].dtype.type(filter_value)
+        if df[self.data_variable].dtype != type(data_value):
+            data_value = df[self.data_variable].dtype.type(data_value)
 
+        # Apply filter and data conditions
+        filter_condition = self.operators[self.filter_operator](df[self.filter_variable], filter_value)
+        data_condition = self.operators[self.data_operator](df[self.data_variable], data_value)
+    
         # Apply combined condition
         filtered_uids = df[filter_condition & data_condition]['UID'].tolist()
         uids[filter_value] = uids.UID.map(set(filtered_uids).__contains__)
-
-        # Sort uids by UID
+    
+         # Sort uids by UID
         uids_sorted = uids.sort_values(by='UID')
 
         return uids_sorted
-
+    
+   
     def __plot(self, uids):
         # Print the results
         total_uids = len(uids)
@@ -77,8 +82,7 @@ class Extractor:
         percentage_true = (total_true / total_uids) * 100
 
         print(f"N of pts with {self.filter_variable} recorded: {total_uids}")
-        print(f"Filter applied: {self.filter_variable} {self.filter_operator} {
-            self.filter_value} AND {self.data_variable} {self.data_operator} {self.data_value} minutes")
+        print(f"Filter applied: {self.filter_variable} {self.filter_operator} {self.filter_value} AND {self.data_variable} {self.data_operator} {self.data_value} minutes")
         print(f"N of pts: {total_true} ")
         print(f"Percentage of pts: {percentage_true:.2f}%")
 
@@ -89,8 +93,6 @@ class Extractor:
         # Plot using Seaborn
         sns.set(style='whitegrid')
         plt.figure(figsize=(4, 4))
-        plt.pie(sizes, explode=[0.1, 0], labels=labels, colors=[
-                '#1E90FF', 'white'], autopct='%1.1f%%', shadow=True, startangle=140, textprops={'fontsize': 9})
-        plt.title(f"{self.filter_value} {self.data_operator} {
-            str(self.data_value)} minutes", fontsize=12, fontweight='bold')
+        plt.pie(sizes, explode=[0.1, 0], labels=labels, colors=['#1E90FF', 'white'], autopct='%1.1f%%', shadow=True, startangle=140, textprops={'fontsize': 9})
+        plt.title(f"{self.filter_value} {self.data_operator} {str(self.data_value)} minutes", fontsize=12, fontweight='bold')
         plt.show()
