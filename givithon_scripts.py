0dbefd2733df7e9f5ab16622b7eed9db7ffde262
Orlando Sagliocco <133375132+kapefier@users.noreply.github.com>
2024-11-07 13:08:33 +0100

    Update givithon_scripts.py

--- givithon_scripts.py
+++ givithon_scripts.py
@@ -13,12 +13,68 @@
 
 import csv
 import pandas as pd
-import matplotlib.pyplot as plt
 # import os
 
 
 def extract(csv_path, uid_path, output_csv, filter_variable, filter_operator, filter_value, data_variable, data_operator, data_value, show=True):
-    # Load data
+    
+    import seaborn as sns
+ 
+    df = pd.read_csv(csv_path)
+    uids = pd.read_csv(uid_path, usecols=[1])
+        
+    operators = {
+        '=': lambda x, y: x == y,
+        '>': lambda x, y: x > y,
+        '<': lambda x, y: x < y,
+        '>=': lambda x, y: x >= y,
+        '<=': lambda x, y: x <= y
+    }
+    
+    if filter_operator not in operators or data_operator not in operators:
+        raise ValueError("Invalid operator. Use one of '=', '>', '<', '>=', '<='.")
+    
+    # Apply filter and data conditions
+    filter_condition = operators[filter_operator](df[filter_variable], filter_value)
+    data_condition = operators[data_operator](df[data_variable], data_value)
+    
+    # Apply combined condition
+    filtered_uids = df[filter_condition & data_condition]['UID'].tolist()
+    uids[filter_value] = uids.UID.map(set(filtered_uids).__contains__)
+    
+    # Sort uids by UID
+    uids_sorted = uids.sort_values(by='UID')
+    
+    # Save the sorted DataFrame to a CSV file
+    uids_sorted.to_csv(output_csv, index=False)
+
+    if show:
+        # Print the results
+        total_uids = len(uids)
+        total_true = uids[filter_value].sum()
+        percentage_true = (total_true / total_uids) * 100
+    
+        print(f"N of pts with {filter_variable} recorded: {total_uids}")
+        print(f"Filter applied: {filter_variable} {filter_operator} {filter_value} AND {data_variable} {data_operator} {data_value} minutes")
+        print(f"N of pts: {total_true} ")
+        print(f"Percentage of pts: {percentage_true:.2f}%")
+
+        # Set Plot
+        labels = ['True', 'False']
+        sizes = [total_true, total_uids - total_true]
+
+        # Plot using Seaborn
+        sns.set(style='whitegrid')
+        plt.figure(figsize=(6, 6))
+        plt.pie(sizes, explode=[0.1, 0], labels=labels, colors=['#1E90FF', 'white'], autopct='%1.1f%%', shadow=True, startangle=140, textprops={'fontsize': 9})
+        plt.title(f"{filter_value} {data_operator} {str(data_value)} minutes", fontsize=12, fontweight='bold')
+        plt.show()
+
+
+def extract_plt(csv_path, uid_path, output_csv, filter_variable, filter_operator, filter_value, data_variable, data_operator, data_value, show=True):
+    
+    import matplotlib.pyplot as plt
+ 
     df = pd.read_csv(csv_path)
     uids = pd.read_csv(uid_path, usecols=[1])
     
